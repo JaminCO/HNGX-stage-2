66ef582f33fb38ba3b85e0e2ba702f9043f0da2a
Jamin Onuegbu <76646536+JaminCO@users.noreply.github.com>
2023-09-11 20:45:14 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.app_context().push()
 app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
--- test.py
+++ test.py
@@ -1,35 +1,35 @@
 import requests
 
 # Define the base URL of your API
-base_url = "https://api.example.com/books"
+base_url = "http://127.0.0.1:5000"
 
 # Function to perform GET request (Read)
-def get_books():
-    response = requests.get(base_url)
+def get_books(id):
+    response = requests.get(f"{base_url}/api/{id}")
     print("GET Request:")
     print(response.status_code)
     print(response.json())
 
 # Function to perform POST request (Create)
-def create_book(title, author):
-    data = {"title": title, "author": author}
-    response = requests.post(base_url, json=data)
+def create_book(name):
+    data = {"name": name}
+    response = requests.post(f"{base_url}/api", json=data)
     print("POST Request:")
     print(response.status_code)
     print(response.json())
 
 # Function to perform PUT request (Update)
-def update_book(book_id, new_title, new_author):
-    url = f"{base_url}/{book_id}"
-    data = {"title": new_title, "author": new_author}
+def update_book(user_id, name):
+    url = f"{base_url}/api/{user_id}"
+    data = {"name": name}
     response = requests.put(url, json=data)
     print("PUT Request:")
     print(response.status_code)
     print(response.json())
 
 # Function to perform DELETE request (Delete)
-def delete_book(book_id):
-    url = f"{base_url}/{book_id}"
+def delete_book(user_id):
+    url = f"{base_url}/api/{user_id}"
     response = requests.delete(url)
     print("DELETE Request:")
     print(response.status_code)
@@ -37,14 +37,14 @@ def delete_book(book_id):
 
 # Test your API
 if __name__ == "__main__":
-    # Test GET request
-    get_books()
-
     # Test POST request
-    create_book("Sample Book 1", "John Doe")
+    create_book("John Doe")
+
+    # Test GET request
+    get_books(1)
 
     # Test PUT request
-    update_book(1, "Updated Book Title", "Jane Smith")
+    update_book(1, "Jane Smith")
 
     # Test DELETE request
     delete_book(1)
